3fa8d7fc81226e59f510f9ca6bb4f6115d83248a
shaw2065 <xh2065@nyu.edu>
2024-02-12 23:06:16 -0500

    in collaboration with Julia Yi: we dicuss & proofread code after writing them independently

--- analyze.py
+++ analyze.py
@@ -1,1 +1,31 @@
 # Place code below to do the analysis part of the assignment.
+import csv
+
+def compute_decade(annmean):
+    return (sum(annmean)/len(annmean))
+
+f = open('./data/clean_data.csv', 'r')
+csv_reader = csv.DictReader(f)
+
+i = 0
+annmean = []
+month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+for line in csv_reader:
+    if i == 10:
+        i = 0
+    if i < 10:
+        for m in month:
+            try:
+                annmean.append(float(line[m]))
+            except:
+                continue
+        if i == 0:
+            starting = line["Year"]
+        elif i == 9:
+            ending = line["Year"]
+            print(starting+" to "+ending+": "+ format(compute_decade(annmean),'.2f')+"°F")
+            annmean = []
+    i += 1
+ending = line["Year"]
+print(starting+" to "+ending+": "+ format(compute_decade(annmean),'.2f')+"°F")
--- munge.py
+++ munge.py
@@ -1,1 +1,39 @@
 # Place code below to do the munging part of this assignment.
+def conver(temperature):
+    return ((temperature/100) * 1.8)
+
+def clean():
+    f = open("./data/GLB.Ts+dSST.txt", "r")
+    w = open("./data/clean_data.csv", "w")
+    heading = False
+    for line in f:
+        if line[0:4] == "Year":
+            if not heading:
+                heading = True
+                h = ""
+                data = line.split()
+                for i in range(len(data) - 1):
+                    if i == 0:
+                        h += data[i]
+                    else:
+                        h += "," + data[i]
+                h += "\n"
+                w.write(h)
+        elif line[0:4].isdigit():
+            t = ""
+            data = line.split()
+            for i in range(len(data) - 1):
+                if i == 0:
+                    t += data[i]
+                else:
+                    if data[i][0:1] != "*":
+                        num = conver(float(data[i]))
+                        num_ = format(num,'.1f')
+                        t += "," + num_
+                    else:
+                        t += ",N/A"
+            t += "\n"
+            w.write(t)
+    w.close()
+
+clean()
